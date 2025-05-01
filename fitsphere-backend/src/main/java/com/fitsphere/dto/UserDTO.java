bce55b05079b2988f596b02a56250e86ee7b8f80
Gayasri72 <gayasripethum1@gmail.com>
2025-05-01 10:28:28 +0530

    updates

--- fitsphere-backend/src/main/java/com/fitsphere/dto/UserDTO.java
+++ fitsphere-backend/src/main/java/com/fitsphere/dto/UserDTO.java
@@ -10,6 +10,14 @@ public class UserDTO {
     private String lastName;
     private String email;
 
+    public UserDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    public UserDTO() {}
+
     public static UserDTO fromUser(User user) {
         UserDTO dto = new UserDTO();
         dto.setId(user.getId());
@@ -18,4 +26,4 @@ public class UserDTO {
         dto.setEmail(user.getEmail());
         return dto;
     }
-} 
+}
